2a936adfeb7491fb3f595cd1a7fc8b8b3990cbb0
Vitor Calejuri <ondeosfrangosnaotemvez@gmail.com>
2010-05-17 20:38:08 -0300

    Fixed bug in CodeMirror editor.

--- templatesadmin/forms.py
+++ templatesadmin/forms.py
@@ -14,21 +14,25 @@ class TemplateForm(forms.Form):
         kwargs.pop('widget_syntax')
 
         super(TemplateForm, self).__init__( *args, **kwargs)
+        print self.fields
 
 
 class RichTemplateForm( forms.Form ):
     """
         Display the code using CodeMirror editor
     """
+    content = forms.CharField(
+        widget = forms.Textarea(attrs={'rows': 10, 'cols': 40})
+    )
 
     def __init__(self, *args, **kwargs):
-        self.syntax = kwargs.pop('widget_syntax') or 'dummy' 
+        syntax = kwargs.pop('widget_syntax') or 'dummy' 
 
         super(RichTemplateForm, self).__init__( *args, **kwargs )
 
         #
-        # Create editor field dynamically 
+        # Overwrite editor field dynamically 
         # 
         self.fields['content'] =  forms.CharField(
-            widget = CodeMirrorEditor(attrs={'syntax': self.syntax})
+            widget = CodeMirrorEditor(attrs={'syntax': syntax})
         ) 
