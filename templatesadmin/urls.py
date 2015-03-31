88a990bb7f63a9cf2ac9cad3e3478249b5a41174
Denis Kopitsa <denis.kopitsa@gmail.com>
2015-03-31 23:17:39 +0300

    Fix conflicts after joining reincubate's fork

--- templatesadmin/urls.py
+++ templatesadmin/urls.py
@@ -1,5 +1,5 @@
-from django.utils.functional import update_wrapper
 from django.contrib.admin.sites import AdminSite
+from django.conf.urls import *
 
 urlpatterns = patterns('',
     url(r'^$', 'templatesadmin.views.overview', name='templatesadmin-overview'),
